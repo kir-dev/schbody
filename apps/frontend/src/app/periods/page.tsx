4b1a3550e9ed7f39222944e07d3b4c2e188a4bc8
mozsarmate <mozsarmatee@gmail.com>
2024-07-12 17:46:42 +0200

    card on top

--- apps/frontend/src/app/periods/page.tsx
+++ apps/frontend/src/app/periods/page.tsx
@@ -3,50 +3,34 @@ import React from 'react';
 import { mockApplications } from '@/app/mockdata/mock-data';
 import { columns } from '@/app/periods/columns';
 import { DataTable } from '@/app/periods/data-table';
+import Th1, { Th2 } from '@/components/typography/typography';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Switch } from '@/components/ui/switch';
 
 export default function Page() {
   const applications = mockApplications;
 
   return (
     <>
-      <h1 className='m-8 text-2xl font-bold'>Jelentkezési időszak kezelése</h1>
+      <Th1 text='Jelentkezési időszak kezelése' />
+      <Card className='m-8'>
+        <CardHeader>
+          <CardTitle>{applications[0].period.name}</CardTitle>
+          <CardDescription>
+            {applications[0].period.applicationStart.toString().slice(0, 16)} -{' '}
+            {applications[0].period.applicationEnd.toString().slice(0, 16)}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className='flex items-center space-x-2'>
+            <Switch id='airplane-mode' />
+            <Label htmlFor='airplane-mode'>Az itt kiosztott belépők jelenleg érvényesek</Label>
+          </div>
+        </CardContent>
+      </Card>
       <div className='m-8'>
-        {/*         <Table className='w-full'>
-          <TableHeader>
-            <TableRow>
-              <TableHead>
-                <Checkbox />
-              </TableHead>
-              <TableHead colSpan={2}>User</TableHead>
-              <TableHead>Státusz</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {applications.map((app) => (
-              <TableRow key={app.id} className='h-0 p-0'>
-                <TableCell className='font-medium py-2'>
-                  <Checkbox />
-                </TableCell>
-                <TableCell className='w-0 py-2'>
-                  <Avatar>
-                    <AvatarImage src='' alt={app.user.nickName.at(0)} />
-                    <AvatarFallback>{app.user.nickName.at(0)}</AvatarFallback>
-                  </Avatar>
-                </TableCell>
-                <TableCell className='py-2'>{app.user.fullName}</TableCell>
-                <TableCell className='py-2'>
-                  <Badge>{app.status}</Badge>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell colSpan={3}>Összesen</TableCell>
-              <TableCell className='text-right'>{applications.length} jelentkezes</TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>*/}
+        <Th2 text='Jelentkezők' />
         <DataTable columns={columns} data={applications} />
       </div>
     </>
