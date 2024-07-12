0de4feec38a2744a1c44aab0d270bee89fd19e02
mozsarmate <mozsarmatee@gmail.com>
2024-07-12 13:54:44 +0200

    types

---
+++ apps/frontend/src/types/application-entity.ts
@@ -0,0 +1,19 @@
+import { ApplicationPeriodEntity } from '@/types/application-period-entity';
+import { UserEntity } from '@/types/user-entity';
+
+enum ApplicationStatus {
+  SUBMITTED,
+  ACCEPTED,
+  REJECTED,
+  NEEDS_REVIEW,
+  FINISHED,
+}
+
+export type ApplicationEntity = {
+  id: number;
+  user: UserEntity;
+  period: ApplicationPeriodEntity;
+  status: ApplicationStatus;
+  createdAt: Date;
+  updatedAt: Date;
+};
