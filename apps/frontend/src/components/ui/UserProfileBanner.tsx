2e1327c1e4b69cfa6fa7ba0d755e4496492aaa05
mozsarmate <mozsarmatee@gmail.com>
2024-08-18 23:37:33 +0200

    refactoring

---
+++ apps/frontend/src/components/ui/UserProfileBanner.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+import { Th2, TTitle } from '@/components/typography/typography';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { UserTimeStampsBlock } from '@/components/ui/UserTimeStampsBlock';
+import { UserEntity } from '@/types/user-entity';
+
+export default function UserProfileBanner(props: {
+  user: UserEntity | undefined;
+  editingIsOn: boolean;
+  onClick: () => void;
+  onSubmit: () => void;
+}) {
+  return (
+    <Card className='mx-8 my-4 flex max-md:flex-col md:flex-row'>
+      <div className='min-w-44 min-h-44 w-1/4 h-full aspect-square relative'>
+        <Image
+          src='https://mozsarmate.me/marci.jpg'
+          placeholder='blur'
+          blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkAAIAAAoAAv/lxKUAAAAASUVORK5CYII='
+          alt='PROFIL KEP'
+          fill
+          className='rounded-l-xl'
+        />
+        <div className='w-full absolute flex bottom-2'>
+          <Button variant='secondary' className='block m-auto'>
+            Profilkép módosítása
+          </Button>
+        </div>
+      </div>
+      <div className='w-full relative'>
+        <CardContent>
+          <div className='flex mt-10 justify-between'>
+            <div className='flex items-start'>
+              <div>
+                <TTitle className='mt-0'>{props.user?.fullName}</TTitle>
+                <Th2 className='ml-8'>{props.user?.neptun}</Th2>
+              </div>
+              {/*{user?.role !== Role.USER && (*/}
+              <Badge className='text-md px-4 py-2 rounded-xl' variant='secondary'>
+                {props.user?.role}
+              </Badge>
+              {/*)}*/}
+            </div>
+            <div className='flex gap-4'>
+              {!props.editingIsOn && (
+                <Button variant='secondary' onClick={props.onClick}>
+                  Adatok szerkesztése
+                </Button>
+              )}
+              {props.editingIsOn && <Button type='submit'>Mentés</Button>}
+              <Link href='/auth/logout'>
+                <Button variant='destructive'>Kijelenkezés</Button>
+              </Link>
+            </div>
+          </div>
+          <UserTimeStampsBlock user={props.user} />
+        </CardContent>
+      </div>
+    </Card>
+  );
+}
--- apps/frontend/src/components/ui/input.tsx
+++ apps/frontend/src/components/ui/input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
     <input
       type={type}
       className={cn(
-        'flex h-10 w-full rounded-md border border-input bg-background shadow-sm px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed  disabled:border-0',
+        'flex h-10 w-full rounded-md border border-input bg-background shadow-sm px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:shadow-none disabled:font-bold disabled:border-0',
         className
       )}
       ref={ref}
