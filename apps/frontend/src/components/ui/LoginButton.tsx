5061a372ffc446ea1994f1f98a25f981dcbecd8d
mozsarmate <mozsarmatee@gmail.com>
2024-08-21 00:12:40 +0200

    header for mobile screens

--- apps/frontend/src/components/ui/LoginButton.tsx
+++ apps/frontend/src/components/ui/LoginButton.tsx
@@ -1,10 +1,16 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import { FiLogIn, FiUser } from 'react-icons/fi';
 
 import { Button } from '@/components/ui/button';
 import useProfile from '@/hooks/useProfile';
 
-export default function LoginButton() {
+enum aversion {
+  LARGE,
+  ICON,
+}
+
+export default function LoginButton({ version }: { version: number }) {
   const { data: user } = useProfile();
   const router = useRouter();
 
@@ -20,12 +26,19 @@ export default function LoginButton() {
     <>
       {user && (
         <Button className='m-8 ml-0' onClick={handleNavToProfile}>
-          {user.fullName}
+          {version === 1 && user.fullName}
+          {version === 0 && (
+            <>
+              <FiUser />
+              {user.fullName.slice(0, 1)}
+            </>
+          )}
         </Button>
       )}
       {!user && (
         <Button className='m-8 ml-0 ' onClick={handleLogin}>
-          Bejelentkezés
+          {version === 1 && 'Bejelentkezés'}
+          {version === 0 && <FiLogIn />}
         </Button>
       )}
     </>
--- apps/frontend/src/components/ui/header.tsx
+++ apps/frontend/src/components/ui/header.tsx
@@ -6,11 +6,11 @@ import LoginButton from '@/components/ui/LoginButton';
 export default function Header() {
   return (
     <header>
-      <div className='flex w-full h-16 bg-amber-200 items-center justify-between'>
+      <div className='flex flex-row w-full h-16 bg-amber-200 items-center justify-between'>
         <TTitle>
           <Link href='/'>SchBody</Link>
         </TTitle>
-        <div className='flex items-center gap-8'>
+        <div className='flex items-center max-md:hidden gap-8 h-full'>
           <THeaderLink>
             <Link href='/gym'>Terem</Link>
           </THeaderLink>
@@ -23,7 +23,24 @@ export default function Header() {
           <THeaderLink>
             <Link href='/faq'>GYIK</Link>
           </THeaderLink>
-          <LoginButton />
+          <LoginButton version={1} />
+        </div>
+        <div className='flex items-center max-md:overflow-scroll gap-4 h-full md:hidden'>
+          <THeaderLink>
+            <Link href='/gym'>Terem</Link>
+          </THeaderLink>
+          <THeaderLink>
+            <Link href='/rules'>Szabályzat</Link>
+          </THeaderLink>
+          <THeaderLink>
+            <Link href='/members'>Körtagok</Link>
+          </THeaderLink>
+          <THeaderLink>
+            <Link href='/faq'>GYIK</Link>
+          </THeaderLink>
+        </div>
+        <div className='flex items-center h-16 md:hidden ml-2'>
+          <LoginButton version={0} />
         </div>
       </div>
     </header>
