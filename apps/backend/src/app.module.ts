6d65c9e4dcf32f67de767c3dc2a1dbbe7c829fa6
Bujdosó Gergő <bujgergo@gmail.com>
2024-08-23 10:06:40 +0200

    fixed import sorting

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,8 +3,8 @@ import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApplicationPeriodModule } from './application-period/application-period.module';
 import { ApplicationModule } from './application/application.module';
+import { ApplicationPeriodModule } from './application-period/application-period.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { UserModule } from './user/user.module';
