6c99f152a45f8064e967f98c1dc781a4b8f52e05
Bujdosó Gergő <bujgergo@gmail.com>
2024-08-29 11:15:40 +0200

    added middleware for permission checking

--- apps/frontend/src/lib/utils.ts
+++ apps/frontend/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export const getRoleFromJwt = (jwt?: string): string => {
+  const dataFromjwt = jwt ? JSON.parse(atob(jwt.split('.')[1])) : null;
+  return dataFromjwt ? dataFromjwt.role : 'UNAUTHORIZED';
+};
