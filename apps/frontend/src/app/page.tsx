00ed26229b80eb5182eddb80698310cbb4fc61c4
mozsarmate <mozsarmatee@gmail.com>
2024-08-31 16:12:37 +0200

    hmm

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -4,14 +4,10 @@ import React from 'react';
 import Th1 from '@/components/typography/typography';
 import ApplicationBannerCard from '@/components/ui/ApplicationBannerCard';
 import Forum from '@/components/ui/Forum';
-import { useUsers } from '@/hooks/useUsers';
 
 export default function Home() {
-  const { data } = useUsers();
-
   return (
     <main>
-      {data && data.users && data.users.map((user) => <div key={user.id}>{user.name}</div>)}
       <ApplicationBannerCard />
       <Th1>Hírek</Th1>
       <Forum />
