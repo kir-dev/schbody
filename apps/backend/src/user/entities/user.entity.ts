f066d48c107ef459744ccd272bfcf4e079908a2c
Bujdosó Gergő <bujgergo@gmail.com>
2024-08-31 16:12:53 +0200

    added validation for user entity

--- apps/backend/src/user/entities/user.entity.ts
+++ apps/backend/src/user/entities/user.entity.ts
@@ -1,15 +1,32 @@
+import { Role } from '@prisma/client';
+import { IsBase64, IsBoolean, IsDate, IsEmail, IsEnum, IsNumber, IsPositive, IsString, IsUUID } from 'class-validator';
+
 export class User {
+  @IsUUID()
   authSchId: string;
+  @IsEnum(Role)
   role: string;
+  @IsString()
   fullName: string;
+  @IsEmail()
   email: string;
+  @IsBoolean()
   isSchResident: boolean;
+  @IsBoolean()
   isActiveVikStudent: boolean;
+  @IsNumber()
+  @IsPositive()
   roomNumber: number;
+  @IsBase64()
   profileImage: any;
+  @IsBoolean()
   canHelpNoobs: boolean;
+  @IsString()
   publicDesc: string;
+  @IsDate()
   createdAt: Date;
+  @IsDate()
   updatedAt: Date;
+  @IsDate()
   profileSeenAt: Date;
 }
