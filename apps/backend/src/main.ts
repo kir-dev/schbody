2fc8181c81448ec8e2a80179ecd7298acbefe406
Bujdosó Gergő <bujgergo@gmail.com>
2024-08-31 19:15:34 +0200

    added vercel to cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     })
   );
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3000', '*kir-dev.vercel.app'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     credentials: true,
   });
