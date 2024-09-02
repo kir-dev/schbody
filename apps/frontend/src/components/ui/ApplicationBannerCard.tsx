96f2a4dffb2efcff12940495ed1096f5d3d0fb41
mozsarmate <mozsarmatee@gmail.com>
2024-09-02 10:33:18 +0200

    submitted application feedback

--- apps/frontend/src/components/ui/ApplicationBannerCard.tsx
+++ apps/frontend/src/components/ui/ApplicationBannerCard.tsx
@@ -4,6 +4,7 @@ import { FiFastForward } from 'react-icons/fi';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import ColoredBadge from '@/components/ui/ColoredBadge';
 import useCurrentApplication from '@/hooks/useCurrentApplication';
 import { useCurrentPeriod } from '@/hooks/usePeriod';
 
@@ -11,26 +12,43 @@ export default function ApplicationBannerCard() {
   const router = useRouter();
   const { data: currentPeriod } = useCurrentPeriod();
   const application = useCurrentApplication();
-  if (!currentPeriod || application.data !== undefined) {
+  if (!currentPeriod) {
     return null;
   }
-  return (
-    <div className='flex mt-8'>
-      <Card className='w-full bg-amber-100'>
+  if (application.data) {
+    return (
+      <Card className='w-full bg-amber-200'>
         <CardHeader className='md:flex-row max-md:flex-col w-full justify-between gap-2 max-md:items-start md:items-center'>
           <div>
-            <CardTitle> Jelentkezés </CardTitle>
+            <CardTitle> Leadott jelentkezés </CardTitle>
             <CardDescription>
-              {' '}
-              Jelenleg folyamatban van a <span className='font-bold'>{currentPeriod!.name}</span> jelentkezési időszak!
+              A most zajló, <span className='font-bold'>{currentPeriod!.name}</span> időszakra már sikeresen
+              jelentkeztél!
             </CardDescription>
           </div>
-          <Button className='max-md:w-full' onClick={() => router.push('/application-form')}>
-            <FiFastForward />
-            Jelentkezés
-          </Button>
+          <div className='flex md:flex-row max-md:flex-col gap-4'>
+            <p>Jelentkezésed státusza: </p>
+            <ColoredBadge status={application.data.status} />
+          </div>
         </CardHeader>
       </Card>
-    </div>
+    );
+  }
+  return (
+    <Card className='w-full bg-amber-200'>
+      <CardHeader className='md:flex-row max-md:flex-col w-full justify-between gap-2 max-md:items-start md:items-center'>
+        <div>
+          <CardTitle> Jelentkezés </CardTitle>
+          <CardDescription>
+            {' '}
+            Jelenleg folyamatban van a <span className='font-bold'>{currentPeriod!.name}</span> jelentkezési időszak!
+          </CardDescription>
+        </div>
+        <Button className='max-md:w-full' onClick={() => router.push('/application-form')}>
+          <FiFastForward />
+          Jelentkezés
+        </Button>
+      </CardHeader>
+    </Card>
   );
 }
