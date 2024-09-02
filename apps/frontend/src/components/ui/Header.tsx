318a996f4a2c7740480ca1684e9bb5f256ef0c89
adamvarga2 <adam.varga@bluebax.com>
2024-09-02 19:37:14 +0200

    feat: hamburger-header-menu v1

--- apps/frontend/src/components/ui/Header.tsx
+++ apps/frontend/src/components/ui/Header.tsx
@@ -1,16 +1,47 @@
+'use client';
+import Hamburger from 'hamburger-react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 import { THeaderLink, TTitle } from '@/components/typography/typography';
 import LoginButton from '@/components/ui/LoginButton';
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header>
       <div className='flex flex-row w-full h-16 bg-amber-200 items-center justify-between'>
         <TTitle>
           <Link href='/'>SCHBody</Link>
         </TTitle>
-        <div className='flex items-center max-md:hidden gap-4 h-full'>
+
+        {/* Mobile view */}
+        <div className='flex items-center gap-2 h-full md:hidden'>
+          <LoginButton version={0} />
+          <Hamburger toggled={isOpen} toggle={setIsOpen} />
+        </div>
+
+        {/* Mobile menu */}
+        {isOpen && (
+          <div className='absolute top-16 left-0 right-0 bg-amber-200 flex flex-col items-start md:hidden z-50'>
+            <THeaderLink>
+              <Link href='/gym'>Terem</Link>
+            </THeaderLink>
+            <THeaderLink>
+              <Link href='/rules'>Szabályzat</Link>
+            </THeaderLink>
+            <THeaderLink>
+              <Link href='/members'>Körtagok</Link>
+            </THeaderLink>
+            <THeaderLink>
+              <Link href='/faq'>GYIK</Link>
+            </THeaderLink>
+          </div>
+        )}
+
+        {/* Desktop view */}
+        <div className='hidden md:flex items-center gap-4 h-full'>
           <THeaderLink>
             <Link href='/gym'>Terem</Link>
           </THeaderLink>
@@ -24,23 +55,6 @@ export default function Header() {
             <Link href='/faq'>GYIK</Link>
           </THeaderLink>
           <LoginButton version={1} />
-        </div>
-        <div className='flex items-center max-md:overflow-scroll gap-4 h-full md:hidden'>
-          <THeaderLink>
-            <Link href='/gym'>Terem</Link>
-          </THeaderLink>
-          <THeaderLink>
-            <Link href='/rules'>Szabályzat</Link>
-          </THeaderLink>
-          <THeaderLink>
-            <Link href='/members'>Körtagok</Link>
-          </THeaderLink>
-          <THeaderLink>
-            <Link href='/faq'>GYIK</Link>
-          </THeaderLink>
-        </div>
-        <div className='flex items-center h-16 md:hidden ml-2'>
-          <LoginButton version={0} />
         </div>
       </div>
     </header>
