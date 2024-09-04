666f5b8bd707f6f6348fd2c86df8d29e401fbd94
mozsarmate <mozsarmatee@gmail.com>
2024-09-05 00:41:21 +0200

    filtering

--- apps/frontend/src/app/periods/[id]/columns.tsx
+++ apps/frontend/src/app/periods/[id]/columns.tsx
@@ -9,29 +9,40 @@ import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, Command
 import { Input } from '@/components/ui/input';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import StatusBadge from '@/components/ui/StatusBadge';
+import { filterByDateRange } from '@/lib/customFilters';
 import { ApplicationEntity2, ApplicationStatus } from '@/types/application-entity';
 
-function SortableFilterableHeader(column: Column<ApplicationEntity2>, title: string) {
+function SortableFilterableHeader(column: Column<ApplicationEntity2>) {
   return (
-    <div className=' flex h-4 items-center gap-1 justify-start'>
-      {title}
+    <div className=' flex items-center justify-start'>
+      {column.id}
       <Button variant='ghost' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
-        <MdSortByAlpha size={20} />
+        <MdSortByAlpha size={16} />
       </Button>
       <Popover>
         <PopoverTrigger>
           {column.getFilterValue() === undefined ? (
-            <MdOutlineFilterAlt size={20} />
+            <MdOutlineFilterAlt size={16} />
           ) : (
-            <MdOutlineFilterAltOff size={20} />
+            <MdOutlineFilterAltOff size={16} />
           )}
         </PopoverTrigger>
         <PopoverContent className='p-3'>
-          <p className='mb-2 font-bold'>Szöveges szűrés</p>
+          <div className='flex w-full justify-between mb-2'>
+            <p className='font-bold'>Szöveg szűrés</p>
+            <p
+              className='underline'
+              onClick={() => {
+                column.setFilterValue(undefined);
+              }}
+            >
+              visszaállítás
+            </p>
+          </div>
           <Input
-            value={column.getFilterValue()?.toString()}
+            value={column.getFilterValue() ? column.getFilterValue()!.toString() : ''}
             onChange={(e) => column.setFilterValue(e.target.value)}
-            placeholder={`Szűrés ${title} alapján`}
+            placeholder={`Szűrés ${column.id.toLowerCase()} alapján`}
           />
         </PopoverContent>
       </Popover>
@@ -40,44 +51,78 @@ function SortableFilterableHeader(column: Column<ApplicationEntity2>, title: str
 }
 
 function DateSortableFilterableHeader(column: Column<ApplicationEntity2>) {
-  const [start, setStart] = useState(column.getFilterValue()?.toString() || '');
-  const [end, setEnd] = useState(column.getFilterValue()?.toString() || '');
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const [start, setStart] = useState(column.getFilterValue()?.start || '2024-01-01T00:00');
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const [end, setEnd] = useState(column.getFilterValue()?.end || '2025-01-01T00:00');
 
   const handleStartChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setStart(e.target.value);
-    column.setFilterValue(e.target.value);
+    const newStart = e.target.value;
+    setStart(newStart);
+    column.setFilterValue((oldValue: { start: string; end: string }) => ({ ...oldValue, start: newStart }));
   };
 
+  // Handler for end date change
   const handleEndChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEnd(e.target.value);
-    column.setFilterValue(e.target.value);
+    const newEnd = e.target.value;
+    setEnd(newEnd);
+    column.setFilterValue((oldValue: { start: string; end: string }) => ({ ...oldValue, end: newEnd }));
   };
-
   return (
-    <Popover>
-      <PopoverTrigger>
-        {column.getFilterValue() === undefined ? <MdOutlineFilterAlt size={20} /> : <MdOutlineFilterAltOff size={20} />}
-      </PopoverTrigger>
-      <PopoverContent>
-        <p className='mb-2 font-bold'>Dátum szűrés</p>
-        <div className='flex gap-2'>
-          <Input
-            type='date'
-            value={start}
-            onChange={handleStartChange}
-            className='date-input'
-            placeholder='Szűrés dátum alapján'
-          />
-          <Input
-            type='date'
-            value={end}
-            onChange={handleEndChange}
-            className='date-input'
-            placeholder='Szűrés dátum alapján'
-          />
-        </div>
-      </PopoverContent>
-    </Popover>
+    <div className='flex h-4 items-center gap-1 justify-start'>
+      {column.id}
+      <Button variant='ghost' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+        <MdSortByAlpha size={16} />
+      </Button>
+      <Popover>
+        <PopoverTrigger>
+          {column.getFilterValue() === undefined ? (
+            <MdOutlineFilterAlt size={16} />
+          ) : (
+            <MdOutlineFilterAltOff size={16} />
+          )}
+        </PopoverTrigger>
+        <PopoverContent className='p-3'>
+          <div className='flex w-full justify-between mb-2'>
+            <p className='font-bold'>Dátum szűrés</p>
+            <p
+              className='underline'
+              onClick={() => {
+                column.setFilterValue(undefined);
+                setStart('2024-01-01T00:00');
+                setEnd('2025-01-01T00:00');
+              }}
+            >
+              visszaállítás
+            </p>
+          </div>
+          <div className='flex gap-2'>
+            <div>
+              <p className='mb-2'>Kezdő dátum</p>
+              <Input
+                type='datetime-local'
+                value={start}
+                onChange={handleStartChange}
+                className='date-input'
+                placeholder='Szűrés dátum alapján'
+              />
+            </div>
+            <div>
+              <p className='mb-2'>Vég dátum</p>
+              <Input
+                type='datetime-local'
+                value={end}
+                onChange={handleEndChange}
+                className='date-input'
+                placeholder='Szűrés dátum alapján'
+              />
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
 
@@ -86,6 +131,8 @@ export const columns: (
 ) => ColumnDef<ApplicationEntity2>[] = (onStatusChange) => [
   {
     id: 'Választ',
+    enableResizing: false, // Disable resizing
+    size: 50, // Fixed size for consistency
     header: ({ table }) => (
       <Checkbox
         checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')}
@@ -104,27 +151,36 @@ export const columns: (
   {
     id: 'Név',
     accessorKey: 'user.fullName',
+    enableResizing: false, // Disable resizing
+    size: 150, // Fixed size for consistency
     header: ({ column }) => {
-      return SortableFilterableHeader(column, 'Név');
+      return SortableFilterableHeader(column);
     },
   },
   {
     id: 'Neptun',
     accessorKey: 'user.neptun',
+    enableResizing: false, // Disable resizing
+    size: 120, // Fixed size for consistency
     header: ({ column }) => {
-      return SortableFilterableHeader(column, 'Neptun');
+      return SortableFilterableHeader(column);
     },
   },
   {
     id: 'Kontakt',
     accessorKey: 'user.email',
+    enableResizing: false, // Disable resizing
+    size: 180, // Fixed size for consistency
     header: ({ column }) => {
-      return SortableFilterableHeader(column, 'Kontakt');
+      return SortableFilterableHeader(column);
     },
   },
   {
     id: 'Leadva',
     accessorKey: 'createdAt',
+    enableResizing: false, // Disable resizing
+    size: 140, // Fixed size for consistency
+    filterFn: filterByDateRange,
     header: ({ column }) => {
       return DateSortableFilterableHeader(column);
     },
@@ -146,8 +202,10 @@ export const columns: (
     id: 'Státusz',
     accessorKey: 'status',
     header: ({ column }) => {
-      return SortableFilterableHeader(column, 'Státusz');
+      return SortableFilterableHeader(column);
     },
+    enableResizing: false,
+    size: 100,
     cell: ({ row }) => {
       const [open, setOpen] = useState(false);
       return (
