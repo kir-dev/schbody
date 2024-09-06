95ee8b190c1b1f7c57bb2ae1b09393504f8ee1b1
mozsarmate <mozsarmatee@gmail.com>
2024-09-06 19:41:37 +0200

    updated look on members page

--- apps/frontend/src/app/members/page.tsx
+++ apps/frontend/src/app/members/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Th1, { Th2 } from '@/components/typography/typography';
 import { BodyMemberTile } from '@/components/ui/body-member-tile';
 import LoadingCard from '@/components/ui/LoadingCard';
 import { useMembers } from '@/hooks/useMembers';
@@ -16,19 +17,23 @@ export default function MembersPage() {
 
   return (
     <main>
-      <h1 className='text-3xl font-bold my-4'>Body körtagok</h1>
+      <Th1>Body körtagok</Th1>
       {isLoading && <LoadingCard />}
 
       {/*canhelpnoobs*/}
-      <h2 className='text-2xl font-bold my-4'>Vállal személyes mentorálást:</h2>
-      <div className='grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-4 border-b-2 pb-4 border-black'>
-        {canHelpNoobsUsers.map((u) => (
-          <BodyMemberTile userEntity={u} key={u.authSchId} />
-        ))}
-      </div>
+      {canHelpNoobsUsers.length > 0 && (
+        <>
+          <Th2>Személyes mentorálást vállaló tagok</Th2>
+          <div className='grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-4 pb-4'>
+            {canHelpNoobsUsers.map((u) => (
+              <BodyMemberTile userEntity={u} key={u.authSchId} />
+            ))}
+          </div>
+          <Th2>További körtagok</Th2>
+        </>
+      )}
 
       {/*cant help noobs*/}
-      <h2 className='text-2xl font-bold my-4'>További körtagok:</h2>
       <div className='grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-4'>
         {cannotHelpNoobsUsers.map((u) => (
           <BodyMemberTile key={u.authSchId} userEntity={u} />
--- apps/frontend/src/components/ui/body-member-tile.tsx
+++ apps/frontend/src/components/ui/body-member-tile.tsx
@@ -2,7 +2,7 @@ import { Mail } from 'lucide-react';
 
 import { MemberEntity } from '@/types/user-entity';
 
-import { Card, CardContent, CardTitle } from './card';
+import { Card, CardContent, CardDescription, CardTitle } from './card';
 
 type Props = {
   userEntity: MemberEntity;
@@ -10,32 +10,28 @@ type Props = {
 
 export function BodyMemberTile({ userEntity }: Props) {
   return (
-    <Card className='p-5 flex w-full'>
+    <Card className='p-4 flex w-full gap-4'>
       <img
         src={`${process.env.NEXT_PUBLIC_API_URL}/users/${userEntity.authSchId}/profile-picture`}
         alt='PROFIL KEP'
-        className='h-36'
+        className='h-36 rounded'
         onError={({ currentTarget }) => {
           currentTarget.onerror = null; // prevents looping
           currentTarget.src = 'default_pfp.jpg';
         }}
       />
-      <div className='flex gap-3 mx-5 flex-col'>
-        <CardTitle className='font-bold flex flex-col'>
-          <h1 className='text-xl font-bold'>{userEntity.nickName}</h1>
-          <h3 className='text-base italic font-light'>{userEntity.fullName}</h3>
-          <a
-            href={`mailto:${userEntity.email}`}
-            className='flex flex-row gap-1.5 mb-3 text-base font-normal items-center mt-1'
-          >
-            <Mail size={21} />
-            {userEntity.email}{' '}
-          </a>
-        </CardTitle>
-
-        <CardContent className='flex flex-col gap-1 mx-0 px-0'>
-          <p>{userEntity.publicDesc}</p>
-        </CardContent>
+      <div className='flex flex-col gap-4'>
+        <div>
+          <CardTitle>{userEntity.nickName}</CardTitle>
+          <CardDescription>{userEntity.fullName}</CardDescription>
+        </div>
+        <a href={`mailto:${userEntity.email}`} className='flex flex-row gap-2 items-center text-sm'>
+          <Mail size={16} />
+          {userEntity.email}{' '}
+        </a>
+        {userEntity.canHelpNoobs && (
+          <CardContent className='text-sm p-0'>{userEntity.publicDesc ? userEntity.publicDesc : '-'}</CardContent>
+        )}
       </div>
     </Card>
   );
