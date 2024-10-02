9285ceacd157dec95bd8fc49d16922beecc44b35
mozsarmate <mozsarmatee@gmail.com>
2024-10-02 14:24:17 +0200

    szigen kivul minden

--- apps/frontend/src/components/ui/RoleBadge.tsx
+++ apps/frontend/src/components/ui/RoleBadge.tsx
@@ -1,22 +1,63 @@
 import { useMemo } from 'react';
+import { FiKey, FiMeh, FiSmile, FiUser } from 'react-icons/fi';
 
 import { Badge } from '@/components/ui/badge';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { Role } from '@/types/user-entity';
 
-export default function RoleBadge({ role }: { role: Role }) {
+export function getRoleBadgeColor(role: Role, bg?: boolean) {
   const convertedRole = role as unknown as Role;
-  const color = useMemo(() => {
+  switch (Role[convertedRole]) {
+    case Role.USER:
+      return bg ? 'bg-blue-400' : 'blue';
+    case Role.BODY_MEMBER:
+      return bg ? 'bg-yellow-400' : 'yellow';
+    case Role.BODY_ADMIN:
+      return bg ? 'bg-red-400' : 'red';
+    case Role.SUPERUSER:
+      return bg ? 'bg-green-400' : 'green';
+    default:
+      return bg ? 'bg-gray-400' : 'gray';
+  }
+}
+
+export default function RoleBadge({ role, short }: { role: Role; short?: boolean }) {
+  const convertedRole = role as unknown as Role;
+  const color = useMemo(() => getRoleBadgeColor(role), [role]);
+  // eslint-disable-next-line no-undef
+  const icon: JSX.Element = useMemo(() => {
     switch (Role[convertedRole]) {
       case Role.USER:
-        return 'blue';
+        return <FiUser />;
       case Role.BODY_MEMBER:
-        return 'yellow';
+        return <FiMeh />;
       case Role.BODY_ADMIN:
-        return 'red';
+        return <FiSmile />;
       case Role.SUPERUSER:
-        return 'green';
+        return <FiKey />;
     }
   }, [role]);
 
-  return <Badge variant={color}>{Role[convertedRole]}</Badge>;
+  if (short) {
+    return (
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Badge variant={color} className='pointer-events-none'>
+              <div className='py-1'>{icon}</div>
+            </Badge>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className='font-sans'>{Role[convertedRole]}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    );
+  }
+  return (
+    <Badge variant={color}>
+      <div className='py-1'>{icon}</div>
+      {!short && Role[convertedRole]}
+    </Badge>
+  );
 }
---
+++ apps/frontend/src/components/ui/table-headers/SortableFilterableHeader.tsx
@@ -0,0 +1,46 @@
+import { Column } from '@tanstack/react-table';
+import React from 'react';
+import { MdOutlineFilterAlt, MdOutlineFilterAltOff, MdSortByAlpha } from 'react-icons/md';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { ApplicationEntity } from '@/types/application-entity';
+
+export function SortableFilterableHeader(column: Column<ApplicationEntity>) {
+  return (
+    <div className=' flex items-center justify-start'>
+      {column.id}
+      <Button variant='ghost' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+        <MdSortByAlpha size={16} />
+      </Button>
+      <Popover>
+        <PopoverTrigger>
+          {column.getFilterValue() === undefined ? (
+            <MdOutlineFilterAlt size={16} />
+          ) : (
+            <MdOutlineFilterAltOff size={16} />
+          )}
+        </PopoverTrigger>
+        <PopoverContent className='p-3'>
+          <div className='flex w-full justify-between mb-2'>
+            <p className='font-bold'>Szöveg szűrés</p>
+            <p
+              className='underline'
+              onClick={() => {
+                column.setFilterValue(undefined);
+              }}
+            >
+              visszaállítás
+            </p>
+          </div>
+          <Input
+            value={column.getFilterValue() ? column.getFilterValue()!.toString() : ''}
+            onChange={(e) => column.setFilterValue(e.target.value)}
+            placeholder={`Szűrés ${column.id.toLowerCase()} alapján`}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
