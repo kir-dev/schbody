932accb42932284e6c492ef65462c16f30d3e8f2
mozsarmate <mozsarmatee@gmail.com>
2024-11-07 18:46:57 +0100

    card redesign, badge, data updates

--- apps/frontend/src/app/impressum/page.tsx
+++ apps/frontend/src/app/impressum/page.tsx
@@ -1,21 +1,56 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { LuGithub, LuUser } from 'react-icons/lu';
+
 import { developers } from '@/components/data/developers';
 import Th1 from '@/components/typography/typography';
+import { Button } from '@/components/ui/button';
+import { Card, CardDescription, CardTitle } from '@/components/ui/card';
 import { DeveloperItem } from '@/components/ui/DeveloperItem';
 
 export default function Page() {
+  const router = useRouter();
   return (
     <>
-      <Th1>Impressum</Th1>
-      <p>Ez az shcbody oldala és mi csináltuk és nagyoj jó lett!!!</p>
-      <p>TODO</p>
       <Th1>Fejlesztők</Th1>
-      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-4'>
+      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {developers.map((dev) => (
           <DeveloperItem key={dev.name} dev={dev} />
         ))}
       </div>
+      <Card className='p-4 flex flex-col gap-4'>
+        <CardTitle>Kir-Dev x SCHBody</CardTitle>
+        <CardDescription>
+          <p>
+            A weboldalt az SchBody felkérésére a Kir-Dev készítette 2024 nyarán. Azóta is folyamatosan fejlesztjük és
+            bővítjük a funkciókat. Köszönjük szépen a segítséget a mindenkinek, aki hozzájárult a projekt sikeréhez.
+          </p>
+          <p>
+            Ha szeretnél bekapcsolódni a fejlesztésbe, vagy csak egy hibát találtál, nyugodtan nyiss egy issue-t / pull
+            request-et GitHub-on vagy keress minket személyesen!
+          </p>
+        </CardDescription>
+
+        <div className='flex gap-4 justify-center'>
+          <Button
+            onClick={() => {
+              router.push('https://github.com/kir-dev/schbody');
+            }}
+            variant='secondary'
+          >
+            <LuGithub /> GitHub
+          </Button>
+          <Button
+            onClick={() => {
+              router.push('https://kir-dev.hu');
+            }}
+            variant='secondary'
+          >
+            <LuUser /> Kir-Dev
+          </Button>
+        </div>
+      </Card>
     </>
   );
 }
--- apps/frontend/src/components/ui/DeveloperItem.tsx
+++ apps/frontend/src/components/ui/DeveloperItem.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { Dev } from '../data/developers';
 import { Badge } from './badge';
 import { Card, CardTitle } from './card';
@@ -6,28 +8,30 @@ type Props = {
   dev: Dev;
 };
 
-export const DeveloperItem = ({ dev: { name, img, tags } }: Props) => {
+export const DeveloperItem = ({ dev: { name, img, tags, github } }: Props) => {
   return (
-    <Card style={{ wordBreak: 'break-word' }} className='p-4 flex w-full gap-4'>
-      <img
-        src={img}
-        alt='PROFIL KEP'
-        className='h-36 rounded'
-        onError={({ currentTarget }) => {
-          currentTarget.onerror = null; // prevents looping
-          currentTarget.src = 'default_pfp.jpg';
-        }}
-      />
-      <div className='flex flex-col gap-4'>
-        <CardTitle>{name}</CardTitle>
-        <div className='flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <Badge key={tag} variant='orange' hover={false}>
-              {tag}
-            </Badge>
-          ))}
+    <Link href={github ? `https://github.com/${github}` : 'https://github.com/kir-dev'}>
+      <Card style={{ wordBreak: 'break-word' }} className='flex w-full rounded-lg p-2'>
+        <img
+          src={img}
+          alt='PROFIL KEP'
+          className='h-36 rounded w-36 object-contain'
+          onError={({ currentTarget }) => {
+            currentTarget.onerror = null; // prevents looping
+            currentTarget.src = 'default_pfp.jpg';
+          }}
+        />
+        <div className='flex flex-col ml-4 mt-2 gap-4'>
+          <CardTitle>{name}</CardTitle>
+          <div className='flex flex-wrap gap-2'>
+            {tags.map((tag) => (
+              <Badge key={tag} variant='orange' hover={false}>
+                {tag}
+              </Badge>
+            ))}
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 };
