c7b14284f2d125718943693956c86818908de326
mozsarmate <mozsarmatee@gmail.com>
2024-11-14 09:26:20 +0100

    minor design updates

--- apps/frontend/src/components/ui/Footer.tsx
+++ apps/frontend/src/components/ui/Footer.tsx
@@ -6,18 +6,18 @@ import KirCard from '@/components/ui/KirCard';
 
 export default function Footer() {
   return (
-    <footer className='bg-amber-200 text-black text-center p-4 flex flex-col gap-4'>
-      <div className='flex w-full gap-4 justify-center'>
-        <Link href='https://www.facebook.com/groups/schbody' target='_blank'>
-          <FaFacebookF size={20} />
-        </Link>
-        <Link href='https://www.instagram.com/sch_body/' target='_blank'>
-          <GrInstagram size={20} />
-        </Link>
-      </div>
-      <p className='text-sm'>&copy; 2024 SCHBody</p>
-      <div className='w-full flex justify-center'>
+    <footer className='bg-amber-200 text-black p-4'>
+      <div className='flex gap-4 container mx-auto items-center justify-between'>
+        <div className='flex gap-4 justify-center'>
+          <Link href='https://www.facebook.com/groups/schbody' target='_blank'>
+            <FaFacebookF size={20} />
+          </Link>
+          <Link href='https://www.instagram.com/sch_body/' target='_blank'>
+            <GrInstagram size={20} />
+          </Link>
+        </div>
         <KirCard />
+        <p className='text-sm'>&copy; 2024 SCHBody</p>
       </div>
     </footer>
   );
