d486b999f2081e41c06b2db782873ecebc4dd770
Máté Mozsár <mozsar@kth.se>
2025-12-02 11:45:24 +0100

    impressum 2025

---
+++ apps/frontend/src/app/impressum/2025/page.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import React from 'react';
+import { LuArrowRight, LuBaby, LuGithub, LuRocket, LuRotateCw, LuUser, LuUserPlus } from 'react-icons/lu';
+
+import { developers } from '@/components/data/developers';
+import Th1 from '@/components/typography/typography';
+import { BodyMemberTile } from '@/components/ui/BodyMemberTile';
+import { Button } from '@/components/ui/button';
+import { Card, CardDescription, CardTitle } from '@/components/ui/card';
+import { DeveloperItem } from '@/components/ui/DeveloperItem';
+import LoadingCard from '@/components/ui/LoadingCard';
+import { useMembers } from '@/hooks/useMembers';
+
+export default function Page() {
+  const { data: bodymembers, isLoading } = useMembers();
+  return (
+    <>
+      <Th1>2025</Th1>
+      <Card className='p-4 flex flex-col gap-4'>
+        <CardTitle>Kir-Dev x SCHBody</CardTitle>
+        <p>
+          A weboldalt az SchBody felkérésére a Kir-Dev készítette 2024 nyarán. Azóta is folyamatosan fejlesztjük és
+          bővítjük a funkciókat. Ha szeretnél bekapcsolódni a fejlesztésbe, vagy csak egy hibát találtál, nyugodtan
+          nyiss egy issue-t / pull request-et GitHub-on vagy keress minket személyesen!
+        </p>
+
+        <div className='flex gap-4 justify-center'>
+          <Button
+            onClick={() => {
+              window.open('https://github.com/kir-dev/schbody');
+            }}
+            variant='secondary'
+          >
+            <LuGithub /> GitHub
+          </Button>
+          <Button
+            onClick={() => {
+              window.open('https://kir-dev.hu');
+            }}
+            variant='secondary'
+          >
+            <LuUser /> Kir-Dev
+          </Button>
+        </div>
+      </Card>
+      <Card className='p-4 flex flex-col'>
+        <CardTitle>2025 számokban</CardTitle>
+        <CardDescription>Tiszt</CardDescription>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-8 text-center'>
+          <StatCard title='alkalommal látogattátok meg az oldalt' value={70698} icon={<LuRocket />} />
+          <StatCard title='-an csatlakoztatok az oldalhoz' value={702} icon={<LuUserPlus />} />
+          <StatCard title='felhasználóval rendelkezünk' value={2053} icon={<LuUser />} />
+          <StatCard title='jelentkezést adtatok le idén' value={2457} icon={<LuArrowRight />} />
+          <StatCard title='vicces profilképet utasítuttunk vissza' value={78} icon={<LuBaby />} />
+          <StatCard title='alkalommal halt meg KSZK-s szerverünk, Junior' value={0} icon={<LuRotateCw />} />
+        </div>
+        <div className='text-center w-full italic font-mono'>
+          <span>az kom</span>
+        </div>
+      </Card>
+
+      <Card className='p-4'>
+        <CardTitle>Fejlesztők</CardTitle>
+        <div className='grid  grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8'>
+          {developers.map((dev) => (
+            <DeveloperItem key={dev.name} dev={dev} />
+          ))}
+        </div>
+      </Card>
+      <Card className='p-4'>
+        <CardTitle>Bodysok</CardTitle>
+        {isLoading && <LoadingCard />}
+        {bodymembers && (
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 3xl:grid-cols-6 gap-4 mt-8'>
+            {bodymembers.map((member) => (
+              <BodyMemberTile key={member.authSchId} userEntity={member} simple />
+            ))}
+          </div>
+        )}
+      </Card>
+    </>
+  );
+}
+
+function StatCard({ title, value, icon }: { title: string; value: number; icon: React.ReactNode }) {
+  return (
+    <Card className='py-2 sm:py-4 md:py-8 px-2'>
+      <span className='font-bold text-4xl md:text-6xl flex items-center gap-2 justify-center'>
+        {icon}
+        {value > 10000 ? `${value / 1000}k` : value}
+      </span>
+      <p>{title}</p>
+    </Card>
+  );
+}
--- apps/frontend/src/components/ui/SecondBannerCard.tsx
+++ apps/frontend/src/components/ui/SecondBannerCard.tsx
@@ -10,16 +10,17 @@ export default function SecondBannerCard() {
   //get current date
   const today = new Date();
   if (today.getMonth() !== 12 - 1) return null;
+  const year = today.getFullYear();
   return (
     <Card className='w-full my-4'>
       <CardHeader className='md:flex-row max-md:flex-col w-full justify-between gap-2 max-md:items-start md:items-center'>
         <div className='flex flex-col gap-2'>
-          <CardTitle> The 2024 season comes to an end </CardTitle>
+          <CardTitle> The {year} season comes to an end </CardTitle>
           <CardDescription>Nézd meg, mi történt Body weben az elmúlt időszakban!</CardDescription>
         </div>
-        <Button className='max-md:w-full' onClick={() => router.push('/impressum/2024')}>
+        <Button className='max-md:w-full' onClick={() => router.push('/impressum/' + year)}>
           <LuFastForward />
-          2024 Recap
+          {year} Recap
         </Button>
       </CardHeader>
     </Card>
